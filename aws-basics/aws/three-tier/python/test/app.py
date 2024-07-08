50551d94eb35b4aa477f30e8bb62a9a06adf0277
aadmin <admin@gmail.com>
2024-07-08 11:49:04 +0530

    pushing the working code of both web applications

---
+++ aws-basics/aws/three-tier/python/test/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://master:master1234@172.17.0.2/pdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    distance = db.Column(db.Integer, nullable=False)
+    radius = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name, distance, radius, mass):
+        self.name = name
+        self.distance = distance
+        self.radius = radius
+        self.mass = mass
+
+@app.route("/")
+def index():
+    planets = Planet.query.all()
+    return render_template('index.html', planets=planets)
+
+if __name__ == "__main__":
+    app.run(debug=True)
